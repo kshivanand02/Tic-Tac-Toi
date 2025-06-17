e77d56e073d3c77af20fc467455b209e528c36af
Shivanand <151640209+kshivanand02@users.noreply.github.com>
2025-06-17 16:55:47 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 let boxes = document.querySelectorAll('.box');
-let resetButton = document.querySelector('#reset-button'); // Use querySelector for single element
-
+let resetButton = document.querySelector('#reset-button');
 let turno = true;
+let gameOver = false;
 
 const winpatterns = [
   [0, 1, 2],
@@ -14,40 +14,41 @@ const winpatterns = [
   [6, 7, 8]
 ];
 
+const checkWinner = () => {
+  for (let pattern of winpatterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
+      console.log("Winner:", pos1);
+      alert(`Winner is ${pos1.toUpperCase()}!`);
+      gameOver = true;
+
+      // Optionally disable all boxes after win
+      boxes.forEach(box => box.disabled = true);
+      break;
+    }
+  }
+};
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    console.log("button clicked");
-    if (turno) {
-      box.innerText = 'o';
-      turno = false;
-    } else {
-      box.innerText = 'x';
-      turno = true;
+    if (box.innerText === "" && !gameOver) {
+      box.innerText = turno ? 'o' : 'x';
+      box.disabled = true;
+      checkWinner();
+      turno = !turno;
     }
-    box.disabled = true;
   });
 });
 
-// Reset button functionality
 resetButton.addEventListener("click", () => {
   boxes.forEach((box) => {
     box.innerText = '';
-    box.disabled = false; // Reset the disabled state
+    box.disabled = false;
   });
-  turno = true; // Reset the turn
+  turno = true;
+  gameOver = false;
   console.log("Game reset");
 });
-const checkWinner=() =>{
-    for(let pattern of winpatterns){
-        
-              let posval1= boxes[pattern[0]].innerText;
-              let posval2=  boxes[pattern[1]].innerText;
-              let posval3=  boxes[pattern[2]].innerText;
-              if (posval1!=""&& posval2!=""&& posval3!="") {
-                        if (posval1===posval2&&posval1===posval3){
-                              console.log("winner",posval1);
-        }
-    }  
-
-    }
-}; 
